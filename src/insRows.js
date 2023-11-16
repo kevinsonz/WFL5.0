8351fd509493a585ea4dbc2f8dbbb3e8346fd379
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-16 23:40:29 +0900

    #4 空白チェックではなく、runFlugへ切替え。

--- src/insRows.js
+++ src/insRows.js
@@ -10,24 +10,16 @@ const endRow = gssSheet.getMaxRows();
 const endCol = gssSheet.getMaxColumns();
 const fomulaCols = [2,3,4,14,15,16,17,18,19,20,21,22,23];
 
-// 行列
-const llMatrix = gssSheet.getRange(8,1,endRow,endCol).getValues();
-
-// 空白チェック
-let blankCheck = false;
-for(i=0; i<endCol; i++){
-    if(llMatrix[i][1]===''){
-        blankCheck = true;
-        break;
-    }
-}
+// フラグ
+const runFlag = gssSheet.getRange('A2').getValue();
 
 // 数式リセット
 function insRows(){
-    if(blankCheck){
+    if(runFlag){
         for(i=0; i<fomulaCols.length; i++){
-            const llFormula = gssSheet.getRange(8,fomulaCols[i]).getFormula();
-            gssSheet.getRange(8,fomulaCols[i],endRow-beginRow+1,1).setFormula(llFormula);
+            const llFormula = gssSheet.getRange(beginRow,fomulaCols[i]).getFormula();
+            gssSheet.getRange(beginRow,fomulaCols[i],endRow-beginRow+1,1).setFormula(llFormula);
         }
+        gssSheet.getRange('A2').setValue(false);
     }
 }
