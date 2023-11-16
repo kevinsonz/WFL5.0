338a6a5e63a75f19dcd28f5c02e07f662aeec4e8
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-16 23:03:56 +0900

    #4 最下行数補正2（+1）

--- src/insRows.js
+++ src/insRows.js
@@ -12,6 +12,6 @@ let fomulaCols = [2,3,4,14,15,16,17,18,19,20,21,22,23];
 function insRows(){
     for(i=0;i<fomulaCols.length;i++){
         const llFormula = gssSheet.getRange(8,fomulaCols[i]).getFormula();
-        gssSheet.getRange(8,fomulaCols[i],endRow-beginRow,1).setFormula(llFormula);
+        gssSheet.getRange(8,fomulaCols[i],endRow-beginRow+1,1).setFormula(llFormula);
     }
 }
