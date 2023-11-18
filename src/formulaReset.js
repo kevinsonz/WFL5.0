e25624616c8f184205325c5830b09cd2af8348a0
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-18 12:21:54 +0900

    #12 微修正（シート名指定の"を'に変更。）

--- src/formulaReset.js
+++ src/formulaReset.js
@@ -1,7 +1,7 @@
 // ファイル・シート
 const gssFile = SpreadsheetApp.getActiveSpreadsheet();
 const gssSheet = gssFile.getActiveSheet();
-const templateSheet = gssFile.getSheetByName("雛型");
+const templateSheet = gssFile.getSheetByName('雛型');
 
 // 行
 const beginRow = 8;
