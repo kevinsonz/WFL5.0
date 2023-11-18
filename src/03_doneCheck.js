572a3db9fdabe3f0b402d5cb560d67c6eee62384
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-18 21:22:37 +0900

    #6 Done入力時に実行＋Error時は空白に戻す

--- src/03_doneCheck.js
+++ src/03_doneCheck.js
@@ -2,21 +2,25 @@
 const llData = gssSheet.getRange(beginRow,1,endRow-beginRow+1,endCol).getValues();
 
 // 日ヘッダーエラー行書式設定
-function doneCheck(){
-    if(typeData === 'LL'){
-        for(i=0;i<(endRow-beginRow+1);i++){
-            if(llData[i][0]===true){
-                if(llData[i][15]===0){
-                    if(llData[i][22]<5){
-                        gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('black');
+function doneCheck(e){
+    const doneFlag = e.value === 'Done';
+    if(doneFlag){
+        if(typeData === 'LL'){
+            for(i=0;i<(endRow-beginRow+1);i++){
+                if(llData[i][0]===true){
+                    if(llData[i][15]===0){
+                        if(llData[i][22]<5){
+                            gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('black');
+                            gssSheet.getRange(i+beginRow,1,1,endCol).setFontColor('white');
+                        }else if(llData[i][22]>=5){
+                            gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('#990000');
+                            gssSheet.getRange(i+beginRow,1,1,endCol).setFontColor('white');
+                        }
+                    }else if(llData[i][15]===1){
+                        gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('red');
                         gssSheet.getRange(i+beginRow,1,1,endCol).setFontColor('white');
-                    }else if(llData[i][22]>=5){
-                        gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('#990000');
-                        gssSheet.getRange(i+beginRow,1,1,endCol).setFontColor('white');
+                        gssSheet.getRange(i+beginRow,6).setValue('');
                     }
-                }else if(llData[i][15]===1){
-                    gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('red');
-                    gssSheet.getRange(i+beginRow,1,1,endCol).setFontColor('white');
                 }
             }
         }
