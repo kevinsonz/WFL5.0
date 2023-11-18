c16b3dc106ea63d4b2250a25a7b7ac20b488cd1f
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-18 11:48:14 +0900

    #12 ファイル名変更。runFlag見直し。

---
+++ src/formulaReset.js
@@ -0,0 +1,31 @@
+// ファイル・シート
+const gssFile = SpreadsheetApp.getActiveSpreadsheet();
+const gssSheet = gssFile.getActiveSheet();
+
+// 行
+const beginRow = 8;
+const endRow = gssSheet.getMaxRows();
+
+// 列
+const endCol = gssSheet.getMaxColumns();
+const formulaCols = [2,3,4,5,14,15,16,17,18,19,20,21,22,23];
+
+// 識別
+const typeData = gssSheet.getRange('A1').getValue(); 
+const runFunc = gssSheet.getRange('C2').getValue();
+const runFlag = typeData === 'LL' && runFunc === '数式';
+
+// 数式リセット
+function formulaReset(){
+    if(runFlag){
+        for(i=0; i<formulaCols.length; i++){
+            const llFormula = gssSheet.getRange(beginRow,formulaCols[i]).getFormula();
+            gssSheet.getRange(beginRow,fomulaCols[i],endRow-beginRow+1,1).setFormula(llFormula);
+        } 
+    }
+    gssSheet.getRange('A2').setValue(false);
+}
+
+function onEdit(e){
+    formulaReset();
+}
