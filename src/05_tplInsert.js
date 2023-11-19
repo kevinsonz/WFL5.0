1b76ec0317c0a9c2a48044ebcbff69402cb5f8a9
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-19 14:25:53 +0900

    #5 e.value適用

--- src/05_tplInsert.js
+++ src/05_tplInsert.js
@@ -3,23 +3,22 @@ const tplEndRow = templateSheet.getMaxRows();
 const tplData = templateSheet.getRange(beginRow,1,tplEndRow-beginRow+1,endCol).getValues();
 
 function tplInsert(e){
-    // 'tpl_A' → e.value
     const tplNamesSheet = gssFile.getSheetByName('tplNames');
     const tplNames = tplNamesSheet.getRange('A1:A').getValues().flat();
-    const tplDataCheck = tplNames.includes('tpl_A');
+    const tplDataCheck = tplNames.includes(e.value);
     const runFlag =  tplDataCheck && typeData === 'LL';
     let tplTargetData = [];
     if(runFlag){
         let tplRows = 0;
         for(let i=0;i<(tplEndRow-beginRow+1);i++){
-            if(tplData[i][12]==='tpl_A'){
+            if(tplData[i][12]===e.value){
                 tplTargetData.push(tplData[i]);
                 tplRows = tplRows + 1;
             }
         }
         let insFlagRow = 0;
         for(let i=0;i<(endRow-beginRow+1);i++){
-            if(llData[i][12]==='tpl_A'){
+            if(llData[i][12]===e.value){
                 gssSheet.insertRowsAfter(beginRow+i,tplRows);
                 gssSheet.getRange(beginRow+i+1,1,tplRows,25).setValues(tplTargetData);
                 insFlagRow = (beginRow+i);
