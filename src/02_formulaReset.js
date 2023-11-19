66bfd5e409548373ba09e2614d56862e18be338b
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-19 21:07:23 +0900

    #5 数式リセット_callフラグ

--- src/02_formulaReset.js
+++ src/02_formulaReset.js
@@ -2,12 +2,8 @@
 function formulaReset(call,addRows){
     const formulaCols = [2,3,4,5,14,15,16,17,18,19,20,21,22,23,24,25];
     const runFlag = (runFunc === '数式') && isLL && okCol;
-    if(runFlag){
-        if(call !== 'call'){
-            addRows = 0;
-        }else{
-            addRows = addRows;
-        }
+    const callFlag = (call === 'call') && isLL && okCol;
+    if(runFlag || callFlag){
         for(let i=0; i<formulaCols.length; i++){
             const llFormula = templateSheet.getRange(beginRow,formulaCols[i]).getFormula();
             gssSheet.getRange(beginRow,formulaCols[i],endRow-beginRow+1+addRows,1).setFormula(llFormula);
