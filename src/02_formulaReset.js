9a56555a159c9a99e8e2fb2aac52df7b45bdf8c3
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-19 15:07:35 +0900

    #5 「+addRows」追加

--- src/02_formulaReset.js
+++ src/02_formulaReset.js
@@ -1,11 +1,11 @@
 // 数式リセット
-function formulaReset(call){
+function formulaReset(call,addRows){
     const formulaCols = [2,3,4,5,14,15,16,17,18,19,20,21,22,23,24,25];
     const runFlag = ((runFunc === '数式') || (call === 'call')) && isLL && okCol;
     if(runFlag){
         for(let i=0; i<formulaCols.length; i++){
             const llFormula = templateSheet.getRange(beginRow,formulaCols[i]).getFormula();
-            gssSheet.getRange(beginRow,formulaCols[i],endRow-beginRow+1,1).setFormula(llFormula);
+            gssSheet.getRange(beginRow,formulaCols[i],endRow-beginRow+1+addRows,1).setFormula(llFormula);
         }
         gssSheet.getRange('C2').setValue(''); 
     }
--- src/04_formatReset.js
+++ src/04_formatReset.js
@@ -1,8 +1,8 @@
-function formatReset(call){
+function formatReset(call,addRows){
     const runFlag = ((runFunc === '書式') || (call === 'call')) && isLL && okCol;
     if(runFlag){
-        llData = gssSheet.getRange(beginRow,1,endRow-beginRow+1,endCol).getValues();
-        bgData = gssSheet.getRange(beginRow,1,endRow-beginRow+1,endCol).getBackgrounds();
+        llData = gssSheet.getRange(beginRow,1,endRow-beginRow+1+addRows,endCol).getValues();
+        bgData = gssSheet.getRange(beginRow,1,endRow-beginRow+1+addRows,endCol).getBackgrounds();
         for(let i=0;i<(endRow-beginRow+1);i++){
             if((llData[i][0]===false) && bgData[i][0]!=='white'){
                 gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('white');
