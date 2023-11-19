10f87f0161dc72a98a3fd41edc724bcb5aa2cf51
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-19 14:41:32 +0900

    #11 行数調節

--- src/04_formatReset.js
+++ src/04_formatReset.js
@@ -1,7 +1,7 @@
 function formatReset(call){
     const runFlag = ((runFunc === '書式') || (call === 'call')) && isLL && okCol;
     if(runFlag){
-        gssSheet.getRange(beginRow,1,endRow,endCol).setBackground('white');
-        gssSheet.getRange(beginRow,1,endRow,endCol).setFontColor('black');
+        gssSheet.getRange(beginRow,1,endRow-beginRow+1,endCol).setBackground('white');
+        gssSheet.getRange(beginRow,1,endRow-beginRow+1,endCol).setFontColor('black');
     }
 }
