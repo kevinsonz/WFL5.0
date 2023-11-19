daca9bf4e3fe7aa393d3d83b6d7bafeba7c21f8c
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-19 21:30:43 +0900

    #5 書式リセット調整

--- src/04_formatReset.js
+++ src/04_formatReset.js
@@ -1,13 +1,13 @@
 // 書式リセット
 function formatReset(call,addRows){
-    llData = gssSheet.getRange(beginRow,1,endRow-beginRow+1+addRows,endCol).getValues();
-    bgData = gssSheet.getRange(beginRow,1,endRow-beginRow+1+addRows,endCol).getBackgrounds();
     const runFlag = (runFunc === '書式') && isLL && okCol;
     const callFlag = (call === 'call') && isLL && okCol;
     if(runFlag || callFlag){
         if(call !== 'call'){
             addRows = 0;
         }
+        llData = gssSheet.getRange(beginRow,1,endRow-beginRow+1+addRows,endCol).getValues();
+        bgData = gssSheet.getRange(beginRow,1,endRow-beginRow+1+addRows,endCol).getBackgrounds();    
         for(let i=0;i<(endRow-beginRow+1+addRows);i++){
             if((llData[i][0]===false) && bgData[i][0]!=='white'){
                 gssSheet.getRange(i+beginRow,1,1,endCol).setBackground('white');
