d290ef1ce89891f8a30cf39116f5c6459315e1d6
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-26 09:46:07 +0900

    #25 行削除にif適用。

--- src/06_pastLog.js
+++ src/06_pastLog.js
@@ -2,7 +2,9 @@ const bkSheet = gssFile.getSheetByName('bkLog');
 
 function bkLog(){
     const endBkRow = bkSheet.getMaxRows();
-    bkSheet.deleteRows(2,endBkRow-1);
+    if(endBkRow>1){
+        bkSheet.deleteRows(2,endBkRow-1);
+    }
     bkSheet.getRange('A1:Y1').clearContent();
     // llSheet.getRange(llDataRange).copyTo(bkSheet);
 }
