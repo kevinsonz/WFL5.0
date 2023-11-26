12333b64d7da2d35ae156eaf61ee92f3624eb368
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-26 09:58:35 +0900

    #25 copyToパラメタ見直し

--- src/06_pastLog.js
+++ src/06_pastLog.js
@@ -2,10 +2,11 @@ const bkSheet = gssFile.getSheetByName('bkLog');
 
 function bkLog(){
     const endBkRow = bkSheet.getMaxRows();
+    const bkPasteRange = bkSheet.getRange(1,1,endRow-beginRow+1,endCol);
     if(endBkRow>1){
         bkSheet.deleteRows(2,endBkRow-1);
     }
     bkSheet.getRange('A1:Y1').clearContent();
     bkSheet.insertRowsAfter(1,endRow-beginRow);
-    llSheet.getRange(llDataRange).copyTo(bkSheet);
+    llSheet.getRange(llDataRange).copyTo(bkPasteRange);
 }
