78d5c1a7252b30a93bc61f7cd0a5168e3e2946dd
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2023-11-26 09:43:28 +0900

    #25 clearContentテスト

--- src/06_pastLog.js
+++ src/06_pastLog.js
@@ -3,5 +3,6 @@ const bkSheet = gssFile.getSheetByName('bkLog');
 function bkLog(){
     const endBkRow = bkSheet.getMaxRows();
     bkSheet.deleteRows(2,endBkRow-1);
+    bkSheet.getRange('A1:Y1').clearContent();
     // llSheet.getRange(llDataRange).copyTo(bkSheet);
 }
