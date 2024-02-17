ffc945ec6508d47bed1a81f22d73dd2d2ed2ff91
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-02-17 22:05:51 +0900

    #32 所々順番入替え

--- src/03_templateSet.js
+++ src/03_templateSet.js
@@ -8,33 +8,33 @@ function tplInsert(e){
     const runFlag =  tplDataCheck && typeData === 'LL';
     let tplTargetData = [];
     const owFlag = e.slice(0,1) === '_'; // 上書き(OverWrite)フラグ
-        if(runFlag){
+    if(runFlag){
         let tplRows = 0;
         for(let i=0; i<(endRow_tpl-beginRow_LL+1); i++){
             if(tplData[i][13]===e.value){
+                tplRows = tplRows + 1; // 該当するテンプレの行数をカウント
                 if(owFlag){
                     for(let j=6; j<13; j++){
                         tplTargetData.push(tplData[i][j]);
                     }
-                    tplRows = tplRows + 1; // 該当するテンプレの行数をカウント
                     break; // 上書き属性の場合は1行目のみとして次の処理へ進む
                 }else{
                     tplTargetData.push(tplData[i]); // 該当するテンプレを配列に追加
-                    tplRows = tplRows + 1; // 該当するテンプレの行数をカウント
                 }
             }
         }
         let insFlagRow = 0;
         for(let i=0; i<(endRow_LL-beginRow_LL+1); i++){
             if(llData[i][13]===e.value){
+                insFlagRow = (beginRow_LL+i); // テンプレ指定の位置を把握
                 if(owFlag){
                     wflSheet.getRange(beginRow_LL+i,7,1,7).setValues(tplTargetData); // テンプレ貼付
                     wflSheet.getRange(insFlagRow,14,tplRows,1).setValue(''); // テンプレ指定のフラグを初期化
-                    break;
+                    break; // 上書き属性の場合は1行目のみとして次の処理へ進む
                 }else{
                     wflSheet.insertRowsAfter(beginRow_LL+i,tplRows); // テンプレ指定した位置の下へ空白行を挿入
                     wflSheet.getRange(beginRow_LL+i+1,1,tplRows,26).setValues(tplTargetData); // テンプレ貼付 
-                }insFlagRow = (beginRow_LL+i); // テンプレ指定の位置を把握 
+                }
             }
         }
         wflSheet.getRange(insFlagRow,14,tplRows+1,1).setValue(''); // テンプレ指定のフラグを初期化
