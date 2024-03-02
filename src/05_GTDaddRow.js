6365f8c68dae9981cd57436abd3bd2bc2d88ab0d
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-02 13:11:47 +0900

    #48 開始行バグ修正2

--- src/05_GTDaddRow.js
+++ src/05_GTDaddRow.js
@@ -10,7 +10,7 @@ function addRowGTD(){
         gtdSheet.getRange(beginRow_GTD-1,1,endRow_GTD,11).createFilter();
         const maxNo = Math.max.apply(null,gtdNo);
         let addNo = 0;
-        for(i=0;i<(endRow_GTD-1);i++){
+        for(i=0;i<((endRow_GTD)-(beginRow_GTD)+1);i++){
             if(gtdData[i][10] === ''){
               addNo = addNo + 1 ;
               gtdSheet.getRange(beginRow_GTD+i,1).setValue(maxNo + addNo);
@@ -18,7 +18,7 @@ function addRowGTD(){
               gtdSheet.getRange(beginRow_GTD+i,6).setValue('－');
               gtdSheet.getRange(beginRow_GTD+i,7).setFormula('=iferror(if(or(I'+(beginRow_GTD+i)+'="完了",I'+(beginRow_GTD+i)+'="保留",I'+(beginRow_GTD+i)+'="中止"),9,ifs(and(E'+(beginRow_GTD+i)+'=E$1,F'+(beginRow_GTD+i)+'=F$1),1,and(E'+(beginRow_GTD+i)+'=E$1,F'+(beginRow_GTD+i)+'<>F$1),2,and(E'+(beginRow_GTD+i)+'<>E$1,F'+(beginRow_GTD+i)+'=F$1),3,and(E'+(beginRow_GTD+i)+'<>E$1,F'+(beginRow_GTD+i)+'<>F$1),4)),9)');
               gtdSheet.getRange(beginRow_GTD+i,9).setValue('未着');
-              gtdSheet.getRange(beginRow_GTD+i,11).setFormula('=countifs(A$2:A,A'+(beginRow_GTD+i)+')');
+              gtdSheet.getRange(beginRow_GTD+i,11).setFormula('=countifs(A$'+beginRow_GTD+':A,A'+(beginRow_GTD+i)+')');
             }
         }
     }
