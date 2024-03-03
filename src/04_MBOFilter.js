a5c7d436ffa7fc7936c8c6bc4a5b997485df056e
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-03 23:04:58 +0900

    #35 全体的に見直し

--- src/04_MBOFilter.js
+++ src/04_MBOFilter.js
@@ -1,17 +1,20 @@
 // MBOフィルターのモード切替え
 
 function tplInsert(e){
-    const runFlag = (endCol === mboCol) && (e['value'] === true) && (e['range'] === 'A2');
+  const eRow = e['range'].getRow();
+  const eColumn = e['range'].getColumn();
+    const runFlag = (endCol === mboCol) && (e['value'] === true) && ((eRow === 2) && (eColumn === 1));
     if(runFlag){
+        const ckBox = mboSheet.getRange('A2').getValue();
         let filterGTD = mboSheet.getFilter();
         if(filterGTD !== null){
-            gtdSheet.getFilter().remove();
+            mboSheet.getFilter().remove();
         }
         if(statusMBO === '今'){
             gtdSheet.getRange(1,1,endRow_MBO,mboCol).createFilter();
             //フィルタの条件設定　【キャベツと等しい】
             let rule = SpreadsheetApp.newFilterCriteria()
-            .whenTextEqualTo("Visible")
+            .whenTextEqualTo('Visible')
             .build();
             //リスト範囲にフィルタを設定し、3列目が"キャベツ"の行のみを表示する
             sh.getRange("B" + beginRow_MBO + ":F" + mboRow).createFilter().setColumnFilterCriteria(3,rule);
