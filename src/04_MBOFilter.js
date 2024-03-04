cc29c353200e7ee7d1a8f92de6a974d3af9138ac
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-04 22:19:00 +0900

    #35 完成

--- src/04_MBOFilter.js
+++ src/04_MBOFilter.js
@@ -1,42 +1,26 @@
 // MBOフィルターのモード切替え
 
 function mboFilter(e){
-    const eRow = e['range'].getRow();
-    const eColumn = e['range'].getColumn();
-      const runFlag = (endCol_MBO === mboCol) && (e['value'] === true) && ((eRow === 2) && (eColumn === 1));
-      let ckBox = mboSheet.getRange('A2').getValue();
-      if(runFlag){
-        let filterMBO = mboSheet.getFilter();
-        if(filterMBO !== null){
-            mboSheet.getFilter().remove();
-        }
-        if(statusMBO === '今' && ckBox === true){
-          const rule = SpreadsheetApp.newFilterCriteria()
-          .setHiddenValue('Hidden')
-          .build();
-          mboSheet.getRange(beginRow_MBO-1,1,mboRow,endCol_MBO).createFilter()
-          .setColumnFilterCriteria(2,rule);
-          logSheet.getRange(7,1).setValue("rule");
-          logSheet.getRange(7,2).setValue(rule);
-        }else{
-          mboSheet.getRange(beginRow_MBO-1,1,mboRow,endCol_MBO).createFilter();
-        }
-        mboSheet.getRange('A2').setValue(false);
+  const eRow = e['range'].getRow();
+  const eColumn = e['range'].getColumn();
+  const runFlag = (endCol_MBO === mboCol) && (eRow === 2) && (eColumn === 1);
+  let ckBox = mboSheet.getRange('A2').getValue();
+  if(runFlag){
+    if(ckBox){
+      let filterMBO = mboSheet.getFilter();
+      if(filterMBO !== null){
+        mboSheet.getFilter().remove();
       }
-      const logSheet = wflFile.getSheetByName('Log');
-      logSheet.clear();
-      logSheet.getRange(1,1).setValue("e['value']");
-      logSheet.getRange(2,1).setValue("eRow");
-      logSheet.getRange(3,1).setValue("eColumn");
-      logSheet.getRange(4,1).setValue("statusMBO");
-      logSheet.getRange(5,1).setValue("ckBox");
-      logSheet.getRange(6,1).setValue("endCol_MBO");
-      // logSheet.getRange(7,1).setValue("rule");
-      logSheet.getRange(1,2).setValue(e['value']);
-      logSheet.getRange(2,2).setValue(eRow);
-      logSheet.getRange(3,2).setValue(eColumn);
-      logSheet.getRange(4,2).setValue(statusMBO);
-      logSheet.getRange(5,2).setValue(ckBox);
-      logSheet.getRange(6,2).setValue(endCol_MBO);
-      // logSheet.getRange(7,2).setValue(rule);
+      if(statusMBO === "今"){
+        const rule = SpreadsheetApp.newFilterCriteria()
+        .setHiddenValues(["Hidden"])
+        .build();
+        mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
+        .setColumnFilterCriteria(2,rule);
+      }else{
+        mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter();
+      }
+    mboSheet.getRange('A2').setValue(false);
+    }
   }
+}
