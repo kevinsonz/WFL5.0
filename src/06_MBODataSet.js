20cd5b3f549e77a81439743250afc6083fccae76
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-09 22:48:54 +0900

    #38 mm：2桁数値設定

--- src/06_MBODataSet.js
+++ src/06_MBODataSet.js
@@ -2,7 +2,8 @@
 
 function dataFormulaSet(){
     for(let i=0; i<366; i++){
-        const mm = mboSheet.getRange(208+i,36).getValue().padStart(2,'0');
+        const m = mboSheet.getRange(208+i,36).getValue();
+        const mm = ('00'+m).slice(-2);
         mboSheet.getRange((208+i),44).setFormula('=countifs(\''+mm+'\'!V:V,AP'+(208+i)+',\''+mm+'\'!B:B,FALSE,\''+mm+'\'!J:J,\"<>\")>0');
     }
 }
