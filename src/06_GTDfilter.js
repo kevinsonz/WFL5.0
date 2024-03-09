2069578e99022b67a07ca8a1885f95a305ad886f
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-09 10:28:41 +0900

    #48 eFlug見直し

--- src/06_GTDfilter.js
+++ src/06_GTDfilter.js
@@ -1,5 +1,6 @@
 // GTDフィルター(On/Off切替え)
 
+// 流用：ルール・フィルター設定
 function ruleSet(a,b,c){
   let rule1 = [];
   let rule2 = [];
@@ -19,10 +20,14 @@ function ruleSet(a,b,c){
   .setColumnFilterCriteria(9,rule3);
 }
 
+// メイン：フィルター隠す機能
 function hiddenGTD(e){
+  // 変数（イベントからのインプット用）
   let eRow = 0;
   let eColumn = 0;
   let eFlag = '';
+
+  // フィルター分岐の元ネタ
   const prmCodeW = ['F','L','E','O','Z'];
   const prmCodeF = ['W','L','E','O','Z'];
   const prmCodeL = ['W','F','E','O','Z'];
@@ -35,73 +40,91 @@ function hiddenGTD(e){
   const prmInactive = ['完了','中止','未着','着手'];
   const prmFinished = ['未着','着手','保留','メモ'];
 
+  // フィルター基準値（GSSから取得）
+  const filterPRM1 = gtdSheet.getRange('D1').getValue();
+  const filterPRM2 = gtdSheet.getRange('G1').getValue();
+  const filterPRM3 = gtdSheet.getRange('I1').getValue();
+
+  // フィルター設定値の枠（二次元配列）
+  let ruleSetData1 = [];
+  let ruleSetData2 = [];
+  let ruleSetData3 = [];
+
+  // パラメタによる場合分け（WFLコード）
+  switch(filterPRM1){
+    case 'W':
+      ruleSetData1 = prmCodeW;
+      eFlag = !prmCodeW.includes(e['value']);
+      break;
+    case 'F':
+      ruleSetData1 = prmCodeF;
+      eFlag = !prmCodeF.includes(e['value']);
+      break;
+    case 'L':
+      ruleSetData1 = prmCodeL;
+      eFlag = !prmCodeL.includes(e['value']);
+      break;
+    case 'E':
+      ruleSetData1 = prmCodeE;
+      eFlag = !prmCodeE.includes(e['value']);
+      break;
+    default:
+      break;
+  }
+
+  // パラメタによる場合分け（急重）
+    switch(filterPRM2){
+    case '急重':
+      ruleSetData2 = prmUrgentAndImportant;
+      eFlag = !prmUrgentAndImportant.includes(e['value']);
+      break;
+    case '急':
+      ruleSetData2 = prmUrgent;
+      eFlag = !prmUrgent.includes(e['value']);
+      break;
+    case '重':
+      ruleSetData2 = prmImportant;
+      eFlag = !prmImportant.includes(e['value']);
+      break;
+    case '無印':
+      ruleSetData2 = prmNormal;
+      eFlag = !prmNormal.includes(e['value']);
+      break;
+    default:
+      break;
+  }
+
+  // パラメタによる場合分け（状態）
+  switch(filterPRM3){
+    case '活性':
+      ruleSetData3 = prmActive;
+      eFlag = !prmActive.includes(e['value']);
+      break;
+    case '非活':
+      ruleSetData3 = prmInactive;
+      eFlag = !prmInactive.includes(e['value']);
+      break;
+    case '終了':
+      ruleSetData3 = prmFinished;
+      eFlag = !prmFinished.includes(e['value']);
+      break;
+    default:
+      break;
+  }
+
+  // フィルター処理実行
   if(e === 'call'){
     eRow = 1;
     eColumn = 9;
   }else{
     eRow = e['range'].getRow();
     eColumn = e['range'].getColumn();
-    eFlag = (e['value'] === '完了' || e['value'] === '中止');
   }
   const runFlug = (e === 'call' || eFlag || ((eRow === 1) && ((eColumn === 4) || (eColumn === 7) || (eColumn === 9)))) && (endCol === 11);
   if(runFlug){
-    const filterPRM1 = gtdSheet.getRange('D1').getValue();
-    const filterPRM2 = gtdSheet.getRange('G1').getValue();
-    const filterPRM3 = gtdSheet.getRange('I1').getValue();
     let filterGTD = gtdSheet.getFilter();
     if(filterGTD !== null){
         gtdSheet.getFilter().remove();
-    }
-
-    let ruleSetData1 = [];
-    let ruleSetData2 = [];
-    let ruleSetData3 = [];
-    switch(filterPRM1){
-      case 'W':
-        ruleSetData1 = prmCodeW;
-        break;
-      case 'F':
-        ruleSetData1 = prmCodeF;
-        break;
-      case 'L':
-        ruleSetData1 = prmCodeL;
-        break;
-      case 'E':
-        ruleSetData1 = prmCodeE;
-        break;
-      default:
-        break;
-    }
-
-    switch(filterPRM2){
-      case '急重':
-        ruleSetData2 = prmUrgentAndImportant;
-        break;
-      case '急':
-        ruleSetData2 = prmUrgent;
-        break;
-      case '重':
-        ruleSetData2 = prmImportant;
-        break;
-      case '無印':
-        ruleSetData2 = prmNormal;
-        break;
-      default:
-        break;
-    }
-
-    switch(filterPRM3){
-      case '活性':
-        ruleSetData3 = prmActive;
-        break;
-      case '非活':
-        ruleSetData3 = prmInactive;
-        break;
-      case '終了':
-        ruleSetData3 = prmFinished;
-        break;
-      default:
-        break;
     }
     ruleSet(ruleSetData1,ruleSetData2,ruleSetData3);
   }
