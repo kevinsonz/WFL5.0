a552da9e6b6fd2951fe0ba9dc5fbe8c5a665b8d3
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-10 16:18:34 +0900

    #38 数式を月単位に設置

--- src/06_MBODataSet.js
+++ src/06_MBODataSet.js
@@ -4,9 +4,9 @@ function dataFormulaSet(){
     const mmDays = mboSheet.getRange('AR143:AR154').getValues(); // 月の日数
     const m = mboSheet.getRange('AJ143:AJ154').getValues(); // 月の情報
     let nextRow = 0;
-    for(let i=0; mmDays.length; i++){
+    for(let i=0; i<mmDays.length; i++){
             const mm = ('00'+m[i]).slice(-2);
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula,mmDays[i],1).setFormula('=countifs(\''+mm+'\'!V:V,AP'+(mboRow_DayStart+i)+',\''+mm+'\'!B:B,FALSE,\''+mm+'\'!J:J,\"<>\")>0');
+            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula,mmDays[i],1).setFormula('=countifs(\''+mm+'\'!V:V,AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!B:B,FALSE,\''+mm+'\'!J:J,\"<>\")>0');
             nextRow = nextRow+mmDays[i];
     }
 }
