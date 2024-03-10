1e307a51825e9eac4c0580d62cc10e9a29f299e3
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-10 16:30:48 +0900

    #38 配列関連のバグを修正

--- src/06_MBODataSet.js
+++ src/06_MBODataSet.js
@@ -4,9 +4,9 @@ function dataFormulaSet(){
     const mmDays = mboSheet.getRange('AR143:AR154').getValues(); // 月の日数
     const m = mboSheet.getRange('AJ143:AJ154').getValues(); // 月の情報
     let nextRow = 0;
-    for(let i=0; i<mmDays[0].length; i++){
-            const mm = ('00'+m[0][i]).slice(-2);
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula,Number(mmDays[0][i]),1).setFormula('=countifs(\''+mm+'\'!V:V,AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!B:B,FALSE,\''+mm+'\'!J:J,\"<>\")>0');
-            nextRow = nextRow+mmDays[0][i];
+    for(let i=0; i<mmDays.length; i++){
+            const mm = ('00'+m[i][0]).slice(-2);
+            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula,Number(mmDays[i][0]),1).setFormula('=countifs(\''+mm+'\'!V:V,AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!B:B,FALSE,\''+mm+'\'!J:J,\"<>\")>0');
+            nextRow = nextRow+Number(mmDays[i][0]);
     }
 }
