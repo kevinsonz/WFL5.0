df5d0f1bebd0bb76abb7eb9a6f68bc7bfa0fcfba
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-10 17:13:52 +0900

    #38 6列スライド適用＋アゲサゲ数式

--- src/06_MBODataSet.js
+++ src/06_MBODataSet.js
@@ -14,20 +14,14 @@ function dataFormulaSet(){
                 .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,maxifs(\''+mm+'\'!E:E,\''+mm+'\'!V:V,AP'+(mboRow_DayStart+nextRow)+'),"-")');
             mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+3,Number(mmDays[i][0]),1)
                 .setFormula('=iferror(if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,if((AS'+(mboRow_DayStart+nextRow)+'-offset(AT'+(mboRow_DayStart+nextRow)+',-1,0))*24>=0,(AS'+(mboRow_DayStart+nextRow)+'-offset(AT'+(mboRow_DayStart+nextRow)+',-1,0))*24,"NG"),"-"),"-")');
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+4,Number(mmDays[i][0]),1)
+            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+4,Number(mmDays[i][0]),6)
                 .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,sumifs(\''+mm+'\'!$G:$G,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(AV$2,1,1)),"-")');
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+5,Number(mmDays[i][0]),1)
-                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,sumifs(\''+mm+'\'!$G:$G,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(AW$2,1,1)),"-")');
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+6,Number(mmDays[i][0]),1)
-                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,sumifs(\''+mm+'\'!$G:$G,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(AX$2,1,1)),"-")');
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+7,Number(mmDays[i][0]),1)
-                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,sumifs(\''+mm+'\'!$G:$G,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(AY$2,1,1)),"-")');
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+8,Number(mmDays[i][0]),1)
-                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,sumifs(\''+mm+'\'!$G:$G,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(AZ$2,1,1)),"-")');
-            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+9,Number(mmDays[i][0]),1)
-                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,sumifs(\''+mm+'\'!$G:$G,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(BA$2,1,1)),"-")');
             mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+10,Number(mmDays[i][0]),1)
                 .setFormula('=iferror(if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,AU'+(mboRow_DayStart+nextRow)+'+BA'+(mboRow_DayStart+nextRow)+',"-"),"-")');
+            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+11,Number(mmDays[i][0]),6)
+                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,countifs(\''+mm+'\'!$L:$L,TRUE,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(BC$2,1,1)),"-")');
+            mboSheet.getRange(mboRow_DayStart+nextRow,mboCol_DataFormula+17,Number(mmDays[i][0]),6)
+                .setFormula('=if($AR'+(mboRow_DayStart+nextRow)+'=TRUE,countifs(\''+mm+'\'!$M:$M,TRUE,\''+mm+'\'!$V:$V,$AP'+(mboRow_DayStart+nextRow)+',\''+mm+'\'!$H:$H,mid(BI$2,1,1)),"-")');
             nextRow = nextRow+Number(mmDays[i][0]);
     }
 }
