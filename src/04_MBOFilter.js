714ae3db68b19308b19a912ee6cbb54638600a06
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-20 17:33:30 +0900

    #55 アゲサゲに反応するマクロを追加

--- src/04_MBOFilter.js
+++ src/04_MBOFilter.js
@@ -3,7 +3,9 @@
 function mboFilter(e){
   const eRow = e['range'].getRow();
   const eColumn = e['range'].getColumn();
-  const runFlag = (endCol_MBO === mboCol) && (eRow === 2) && (eColumn === 1);
+  const ePosition = e['range'].getRow() === 1 && e['range'].getColumn() === 1;
+  const eValue = (e['value'] === '両' || e['value'] === '↑' || e['value'] === '↓');
+  const runFlag = (endCol_MBO === mboCol) && (eRow === 2) && (eColumn === 1) && ePosition && eValue;
   let ckBox = mboSheet.getRange('A2').getValue();
   if(runFlag){
     if(ckBox){
@@ -20,7 +22,31 @@ function mboFilter(e){
       }else{
         mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter();
       }
-    mboSheet.getRange('A2').setValue(false);
+      mboSheet.getRange('A2').setValue(false);
+    }
+
+    if(eValue){
+      const rule = SpreadsheetApp.newFilterCriteria()
+      .setHiddenValues([false])
+      .build();
+      switch(statusMBO){
+        case '両':
+          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
+          .setColumnFilterCriteria(24,rule);
+          break;
+        case '↑':
+          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
+          .setColumnFilterCriteria(25,rule);          
+          break;
+        case '↓':
+          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
+          .setColumnFilterCriteria(26,rule);
+          break;
+        default:
+          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter();
+          break;
+      }
+      mboSheet.getRange('A1').setValue(e['oldValue']);
     }
   }
 }
