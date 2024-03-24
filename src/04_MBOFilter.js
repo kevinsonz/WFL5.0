2bd3de32efb8ec50d18efbdbd1446df8e42c2c60
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-03-24 18:57:14 +0900

    #55 MBOフィルタ完成品(+GTDフィルタパラメタ修正)

--- src/04_MBOFilter.js
+++ src/04_MBOFilter.js
@@ -1,46 +1,49 @@
-// MBOフィルターのモード切替え
+// アゲサゲフィルターに使う関数
+function filterAgeSage(colA,colB,ruleA,ruleB){
+  mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
+  .setColumnFilterCriteria(colA,ruleA)
+  .setColumnFilterCriteria(colB,ruleB);
+}
 
+// MBOフィルターのモード切替え
 function mboFilter(e){
-  const eRow = e['range'].getRow();
-  const eColumn = e['range'].getColumn();
-  const ePosition = e['range'].getRow() === 1 && e['range'].getColumn() === 1;
-  const eValue = (e['value'] === '両' || e['value'] === '↑' || e['value'] === '↓');
-  const runFlag = (endCol_MBO === mboCol) && (eRow === 2) && (eColumn === 1) && ePosition && eValue;
-  let ckBox = mboSheet.getRange('A2').getValue();
+  const ePosition1 = e['range'].getRow() === 2 && e['range'].getColumn() === 1;
+  const ePosition2 = e['range'].getRow() === 1 && e['range'].getColumn() === 1;
+  const eValue1 = (statusMBO === '全' || statusMBO === '今') && e['value'] === 'TRUE';
+  const eValue2 = (e['value'] === '両' || e['value'] === '↑' || e['value'] === '↓');
+  const runFlag = (endCol_MBO === mboCol) && (ePosition1 || ePosition2);
   if(runFlag){
-    if(ckBox){
-      let filterMBO = mboSheet.getFilter();
-      if(filterMBO !== null){
-        mboSheet.getFilter().remove();
-      }
-      if(statusMBO === "今"){
-        const rule = SpreadsheetApp.newFilterCriteria()
-        .setHiddenValues(["Hidden"])
-        .build();
-        mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
+    let filterMBO = mboSheet.getFilter();
+    if(filterMBO !== null){
+      mboSheet.getFilter().remove();
+    }
+    let rule = SpreadsheetApp.newFilterCriteria()
+          .setHiddenValues([])
+          .build();
+    if(statusMBO === "今" || e['oldValue'] === '今'){
+        rule = SpreadsheetApp.newFilterCriteria()
+          .setHiddenValues(["Hidden"])
+          .build();
+    }
+    if(eValue1){
+      mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
         .setColumnFilterCriteria(2,rule);
-      }else{
-        mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter();
-      }
       mboSheet.getRange('A2').setValue(false);
     }
-
-    if(eValue){
-      const rule = SpreadsheetApp.newFilterCriteria()
-      .setHiddenValues([false])
-      .build();
-      switch(statusMBO){
+    if(eValue2){
+      logSheet.getRange(20,1).setValue('動いてる'); // 一時的
+      const rule2 = SpreadsheetApp.newFilterCriteria()
+        .setHiddenValues(["false"])
+        .build();
+      switch(e['value']){
         case '両':
-          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
-          .setColumnFilterCriteria(24,rule);
+          filterAgeSage(2,24,rule,rule2);
           break;
         case '↑':
-          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
-          .setColumnFilterCriteria(25,rule);          
+          filterAgeSage(2,25,rule,rule2);
           break;
         case '↓':
-          mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter()
-          .setColumnFilterCriteria(26,rule);
+          filterAgeSage(2,26,rule,rule2);
           break;
         default:
           mboSheet.getRange(beginRow_MBO-1,1,mboRow+1,endCol_MBO).createFilter();
